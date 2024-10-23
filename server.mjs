0561c0fd7b48a23c2439eced3586409a8c8572f2
Caroline Fortune <158789872+komiline@users.noreply.github.com>
2024-10-23 13:17:59 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,10 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import express from "express";
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('project'));
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my server!');
+  res.sendFile(path.join(__dirname, 'project', 'Main.html'));
 });
 
 app.listen(port, () => {
